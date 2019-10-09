9eb9fa75ba528958b942548484da1152c47139ab
Mauro Bussini <zenprogramming-it@gmail.com>
2019-10-09 15:46:45 +0200

    Optimized method InserisciNumeroArbitrarioPersoneInRubrica

--- HelloAcademy/FunzioniRubrica.cs
+++ HelloAcademy/FunzioniRubrica.cs
@@ -6,60 +6,115 @@ namespace HelloAcademy
 {
     public static class FunzioniRubrica
     {
-        public static void InserisciNumeroArbitrarioPersoneInRubrica() 
+        public static void InserisciNumeroArbitrarioPersoneInRubrica()
         {
             //1) Richiedo il numero di persone da inserire
             Console.Write("Quante persone vuoi inserire (da 1 a 9)? ");
-            string totalPersonsString = Console.ReadLine();
-
-            //2) Validazione del valore immesso e conversione in numero
-            int totalPersons = int.Parse(totalPersonsString);
-
-            //3) Se il numero è minore o uguale a zero, o maggiore di 10 esco
-            if (totalPersons < 1 || totalPersons >= 10) 
-            {
-                //Messaggio utente ed uscita
-                Console.WriteLine("Il numero di persone inseribili deve essere da 1 a 9");
-                Console.Write("Premi un pulsante per uscire");
-                Console.ReadKey();
-                return;
-            }
+            int totalPersons = LeggiNumeroInteroDaConsole(1, 9);
 
             //Dimensionamento della rubrica
             Person[] rubrica = new Person[totalPersons];
 
             //4) Itero per il numero di persone richiesto
-            for (int index = 0; index < totalPersons; index++) 
+            for (int index = 0; index < totalPersons; index++)
             {
-                //5) Richiedo il nome e cognome della persona
-                Console.Write("nome: ");
-                var nome = Console.ReadLine();
-                Console.Write("cognome: ");
-                var cognome = Console.ReadLine();
-
-                //6) Creo oggetto Person da inserire in rubrica
-                Person person = new Person 
-                {
-                    FirstName = nome, 
-                    LastName = cognome
-                };
-
-                //7) Aggiungo persona a rubrica
-                rubrica[index] = person;
-
-                //8) Se ho inserito tutte le persone termino il ciclo
+                //Richiamo una funzione a cui passo la rubrica
+                //e l'indice corrente e questa mi aggiunge la persona
+                AggiungiPersonaARubricaInPosizione(rubrica, index);
             }
 
             //9) Itero la rubrica e stampo a video (con for) tutte le persone
-            Console.WriteLine("*** Visualizzazione contenuto rubrica***");
-            for (var index = 0; index < rubrica.Length; index++)
-                Console.WriteLine($"{rubrica[index].FirstName}, {rubrica[index].LastName}");
+            StampaRubrica(rubrica);
 
             //Cerimonia finale
+            ConfermaUscita();
+        }
+
+        private static int LeggiNumeroInteroDaConsole(int minValue, int maxValue)
+        {
+            //Leggo il valore stringa da console
+            string valoreString = Console.ReadLine();
+            int valoreIntero = 0;
+
+            //Predisposizione al fallimento
+            bool isInteger = false;
+            bool isInRange = false;
+
+            do
+            {
+                try
+                {
+                    //Validazione e parsing del valore
+                    valoreIntero = int.Parse(valoreString);
+                    isInteger = true;
+
+                    //Verifico se è nel range
+                    if (valoreIntero >= minValue && valoreIntero <= maxValue) 
+                    {
+                        //imposto il flag IsInRange
+                        isInRange = true;
+                    }
+                }
+                catch (Exception exc)
+                {
+                    //Messaggio di errore
+                    Console.WriteLine("Attenzione! Il valore immesso non è valido!");
+                    Console.Write("Inseriscilo di nuovo: ");
+
+                    //RIchiesta nuovo valore
+                    valoreString = Console.ReadLine();
+
+                    //Ripristino condizioni di predisposizione fallimento iniziali
+                    valoreIntero = 0;
+                    isInteger = false;                    
+                    isInRange = false;
+                }
+            }
+            while (isInteger == false || isInRange == false);
+
+            //Ritorno il valore intero
+            return valoreIntero;
+        }
+
+        private static void ConfermaUscita()
+        {
             Console.Write("Premi un pulsante per uscire");
             Console.ReadKey();
         }
 
+        private static void StampaRubrica(Person[] rubrica)
+        {
+            Console.WriteLine("*** Visualizzazione contenuto rubrica***");
+            for (var index = 0; index < rubrica.Length; index++)
+            {
+                Console.WriteLine($" => {rubrica[index].FirstName}, {rubrica[index].LastName}");
+                //Console.WriteLine(" => " + rubrica[index].FirstName + ", " + rubrica[index].LastName);
+            }
+        }
+
+        private static void AggiungiPersonaARubricaInPosizione(Person[] rubrica, int index)
+        {
+            //5) Richiedo il nome e cognome della persona
+            Console.Write("nome: ");
+            var nome = Console.ReadLine();
+            Console.Write("cognome: ");
+            var cognome = Console.ReadLine();
+
+            //6) Creo oggetto Person da inserire in rubrica
+            Person person = new Person
+            {
+                FirstName = nome,
+                LastName = cognome
+            };
+
+            //7) Aggiungo persona a rubrica
+            rubrica[index] = person;
+
+            //8) Se ho inserito tutte le persone termino il ciclo
+        }
+
+
+        //******************************************************************
         public static void InserisciPersoneEMostraRubrica()
         {
             //Dimensiono array per la rubrica
